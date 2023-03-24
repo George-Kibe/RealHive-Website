a292425510e7391e9440b68b39556214be1bac4f
George-Kibe <georgekibewambui@gmail.com>
2023-03-24 15:28:11 +0300

    refine layout

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-   
-    resolve: {
-      // configuration options
-      //fallback: { "stream": require.resolve("stream-browserify") },
-      fallback: { "stream": false }
-    },
-  };
