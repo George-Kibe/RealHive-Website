7838feef2c52c662bdf6f51a18276c6947c211ed
George-Kibe <georgekibewambui@gmail.com>
2023-08-15 17:34:53 +0300

    user registration route

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-// import { userRoute } from './routes/userRoute.js';
+import { userRoute } from './routes/userRoute.js';
 // import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
@@ -18,5 +18,5 @@ app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// app.use('/api/user', userRoute)
+app.use('/api/users', userRoute)
 // app.use("/api/residency", residencyRoute)
